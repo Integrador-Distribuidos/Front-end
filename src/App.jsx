970168444e168f9cba019bc04f59f5bf5e04053a
ferreira.lima1 <ferreira.lima1@escolar.ifrn.edu.br>
2025-07-23 11:58:43 -0300

    Revert "Merge pull request #5 from Integrador-Distribuidos/feat/search-page"
    This reverts commit 49e8d2f7223c49f83688f94d8797cd210a6d389d, reversing
    changes made to 83c2a883fe0c1d921b7b136dfcef2570b3b26226.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Index.jsx'
+import Login from './pages/login/index.jsx';
+import SignUp from './pages/signUp/index.jsx';
+import ProductDetail from './pages/productdetail/index.jsx';
+import HomePage from './pages/HomePage/index.jsx';
 
 function App() {
   
 
   return (
-    <>
-      <Header />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/product_detail" element={<ProductDetail />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   )
 }
 
