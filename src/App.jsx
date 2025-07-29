03ced4b36f93ed6ef403dc0651c7e6ee6e77342a
ferreira.lima1 <ferreira.lima1@escolar.ifrn.edu.br>
2025-07-29 16:47:10 -0300

    Feat: Criação da página de Resultados de Busca da Loja

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import SignUp from './pages/signUp/index.jsx';
 import ProductDetail from './pages/productdetail/index.jsx';
 import HomePage from './pages/HomePage/index.jsx';
 import SearchPage from './pages/SearchPage/index.jsx';
+import SearchPageStore from './pages/SearchPageStore/index.jsx';
 function App() {
   
-
   return (
     <Router>
       <Routes>
@@ -17,6 +17,7 @@ function App() {
         <Route path="/product_detail" element={<ProductDetail />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/search_page" element={<SearchPage />} />
+        <Route path="/search_page_store" element={<SearchPageStore />} />
       </Routes>
     </Router>
   )
